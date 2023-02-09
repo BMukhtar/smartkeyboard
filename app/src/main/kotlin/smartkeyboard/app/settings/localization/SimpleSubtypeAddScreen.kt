080e84b68751430974499094d140855be4b4c2a9
Bimurat Mukhtar <v.mukhtar.bimurat@reddit.com>
2023-02-09 18:22:07 +0600

    Removed extra code

--- app/src/main/kotlin/smartkeyboard/app/settings/localization/SimpleSubtypeAddScreen.kt
+++ app/src/main/kotlin/smartkeyboard/app/settings/localization/SimpleSubtypeAddScreen.kt
@@ -18,19 +18,9 @@ package smartkeyboard.app.settings.localization
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Button
-import androidx.compose.material.Card
 import androidx.compose.material.ExtendedFloatingActionButton
 import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
@@ -38,56 +28,37 @@ import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.Saver
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.Observer
-import androidx.navigation.NavOptions
+import dev.patrickgold.jetpref.datastore.model.observeAsState
+import dev.patrickgold.jetpref.material.ui.JetPrefAlertDialog
+import dev.patrickgold.jetpref.material.ui.JetPrefListItem
 import smartkeyboard.R
 import smartkeyboard.app.LocalNavController
 import smartkeyboard.app.Routes
 import smartkeyboard.app.florisPreferenceModel
 import smartkeyboard.ime.core.DisplayLanguageNamesIn
 import smartkeyboard.ime.core.Subtype
-import smartkeyboard.ime.core.SubtypeJsonConfig
 import smartkeyboard.ime.core.SubtypeLayoutMap
 import smartkeyboard.ime.core.SubtypeNlpProviderMap
-import smartkeyboard.ime.core.SubtypePreset
-import smartkeyboard.ime.keyboard.LayoutArrangementComponent
-import smartkeyboard.ime.keyboard.LayoutType
 import smartkeyboard.ime.keyboard.extCorePopupMapping
 import smartkeyboard.keyboardManager
 import smartkeyboard.lib.FlorisLocale
 import smartkeyboard.lib.compose.FlorisButtonBar
-import smartkeyboard.lib.compose.FlorisDropdownLikeButton
-import smartkeyboard.lib.compose.FlorisDropdownMenu
 import smartkeyboard.lib.compose.FlorisScreen
 import smartkeyboard.lib.compose.stringRes
 import smartkeyboard.lib.ext.ExtensionComponentName
 import smartkeyboard.lib.observeAsNonNullState
 import smartkeyboard.subtypeManager
-import dev.patrickgold.jetpref.datastore.model.observeAsState
-import dev.patrickgold.jetpref.material.ui.JetPrefAlertDialog
-import dev.patrickgold.jetpref.material.ui.JetPrefListItem
-import kotlinx.serialization.decodeFromString
-import kotlinx.serialization.encodeToString
-import smartkeyboard.ime.keyboard.DefaultComputingEvaluator.subtype
-import smartkeyboard.ime.text.composing.Appender.id
 
 private val SelectComponentName = ExtensionComponentName("00", "00")
 private val SelectNlpProviderId = SelectComponentName.toString()
-private val SelectNlpProviders = SubtypeNlpProviderMap(
-    spelling = SelectNlpProviderId,
-)
 private val SelectLayoutMap = SubtypeLayoutMap(
     characters = SelectComponentName,
     symbols = SelectComponentName,
@@ -99,31 +70,8 @@ private val SelectLayoutMap = SubtypeLayoutMap(
     phone2 = SelectComponentName,
 )
 private val SelectLocale = FlorisLocale.from("00", "00")
-private val SelectListKeys = listOf(SelectComponentName)
 
 private class SubtypeAddState(init: Subtype?) {
-    companion object {
-        val Saver = Saver<SubtypeAddState, String>(
-            save = { editor ->
-                val subtype = Subtype(
-                    id = editor.id.value,
-                    primaryLocale = editor.primaryLocale.value,
-                    secondaryLocales = editor.secondaryLocales.value,
-                    nlpProviders = editor.nlpProviders.value,
-                    composer = editor.composer.value,
-                    currencySet = editor.currencySet.value,
-                    punctuationRule = editor.punctuationRule.value,
-                    popupMapping = editor.popupMapping.value,
-                    layoutMap = editor.layoutMap.value,
-                )
-                SubtypeJsonConfig.encodeToString(subtype)
-            },
-            restore = { str ->
-                val subtype = SubtypeJsonConfig.decodeFromString<Subtype>(str)
-                SubtypeAddState(subtype)
-            },
-        )
-    }
 
     val id: MutableState<Long> = mutableStateOf(init?.id ?: -1)
     val primaryLocale: MutableState<FlorisLocale> = mutableStateOf(init?.primaryLocale ?: SelectLocale)
@@ -176,34 +124,19 @@ private class SubtypeAddState(init: Subtype?) {
 fun SubtypeSimpleAddScreen() = FlorisScreen {
     title = stringRes(R.string.settings__localization__subtype_add_title)
 
-    val selectValue = stringRes(R.string.settings__localization__subtype_select_placeholder)
-    val selectListValues = remember(selectValue) { listOf(selectValue) }
-
     val prefs by florisPreferenceModel()
     val navController = LocalNavController.current
     val context = LocalContext.current
-    val configuration = LocalConfiguration.current
     val lifecycleOwner = LocalLifecycleOwner.current
     val keyboardManager by context.keyboardManager()
     val subtypeManager by context.subtypeManager()
 
     val displayLanguageNamesIn by prefs.localization.displayLanguageNamesIn.observeAsState()
-    val composers by keyboardManager.resources.composers.observeAsNonNullState()
-    val currencySets by keyboardManager.resources.currencySets.observeAsNonNullState()
-    val layoutExtensions by keyboardManager.resources.layouts.observeAsNonNullState()
-    val popupMappings by keyboardManager.resources.popupMappings.observeAsNonNullState()
     val subtypePresets by keyboardManager.resources.subtypePresets.observeAsNonNullState()
-    val id = null
 
     val subtypeEditor = SubtypeAddState(null)
     var primaryLocale by subtypeEditor.primaryLocale
-    //var secondaryLocales by subtypeEditor.secondaryLocales
-    var composer by subtypeEditor.composer
-    var currencySet by subtypeEditor.currencySet
     var popupMapping by subtypeEditor.popupMapping
-    var layoutMap by subtypeEditor.layoutMap
-
-    var showSubtypePresetsDialog by rememberSaveable { mutableStateOf(false) }
     var showSelectAsError by rememberSaveable { mutableStateOf(false) }
     var errorDialogStrId by rememberSaveable { mutableStateOf<Int?>(null) }
 
@@ -252,11 +185,11 @@ fun SubtypeSimpleAddScreen() = FlorisScreen {
 
     content {
         val suggestedPresets = remember(subtypePresets) {
-            val presets = subtypePresets.filter { it.locale.localeTag() in arrayOf("kk", "en", "ru", "tr") }
+            val presets = subtypePresets.filter { it.locale.localeTag() in arrayOf("kk", "en-US", "ru", "tr") }
             presets
         }
         if (suggestedPresets.isNotEmpty()) {
-            Column(modifier = Modifier.padding(vertical = 8.dp, horizontal = 16.dp)) {
+            Column {
                 suggestedPresets.forEach { subtypePreset ->
                     JetPrefListItem(
                         modifier = Modifier.clickable {
